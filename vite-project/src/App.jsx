a9e9d8904759a906fec7c9c407ef82e7d047f4cb
Sahil pawar <63465844+sahilpawar58@users.noreply.github.com>
2024-06-12 17:46:08 +0530

    woking video js

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VideoPlayer from './VideoPlayer'
+import { useRef } from 'react'
+
+function App() {
+  const playerRef = useRef(null)
+  const videoLink = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL"
+      }
+    ],
+      playbackRates: [-10, -5, -2, -1, -0.5, 0.5, 1, 2, 5, 10]
+    
+  }
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+  return (
+    <>
+      <div>
+        <h1>Video player</h1>
+      </div>
+      <VideoPlayer
+      options={videoPlayerOptions}
+      onReady={handlePlayerReady}
+      />
+    </>
+  )
+}
+
+export default App
